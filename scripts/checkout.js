8652ff6d8962e9fbb590cd52b1fd2ce8c7e9322b
rahul jha <irahul.jha.007@gmail.com>
2025-03-19 05:00:24 +0530

    delete the product when click delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -17,7 +17,7 @@ products.forEach((product) => {
 cartSummaryHTML +=
 
   `
-  <div class="cart-item-container">
+  <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -102,5 +102,8 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
     const productId = link.dataset.productId;
       console.log(productId);
 removeFromCart(productId);
+const container = document.querySelector(`.js-cart-item-container-${productId}`
+);
+container.remove();
   });
 });
